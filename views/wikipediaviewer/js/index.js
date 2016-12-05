45f02f885e4e0f15912d1be7ed6d31b11afcbce1
David Jaimes <davidjaimesdesign@gmail.com>
2016-12-04 20:18:01 -0800

    Open weather api is broken switching to wikipedia viewer

---
+++ views/wikipediaviewer/js/index.js
@@ -0,0 +1,30 @@
+//wiki search api: https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&utf8=1&srsearch=potato+canon
+
+$("#random").on('click',function(){window.location.href='https://en.wikipedia.org/wiki/Special:Random';})
+$("#searchWiki").submit(function(q){
+  q.preventDefault();
+  var queryRaw = $("#searchWiki").serializeArray();
+  var query = (queryRaw[0].value).replace(/ /g,"+");
+  $.ajax({
+    type: 'GET',
+    url: ' https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&utf8=1',
+    contentType: "application/json; charset=utf-8",
+    async: false,
+    dataType: 'jsonp',
+    data: {
+      srsearch: query
+      },
+    success:function(data){
+      var dataArray = data.query.search;
+      console.log(data)
+      for(i = 0; i < dataArray.length; i++){
+        var entryTitle   = dataArray[i].title;
+        var entrySnippet = dataArray[i].snippet;
+        var link         = "https://en.wikipedia.org/wiki/" + entryTitle;
+        var divAdd = ' <a href='+ link +'><div class="container"> <div class="row result top-buffer"> <div class="col-md"> <h4 class = "resultText col-md-3 top-buffer">' + entryTitle + '</h4> <p class = "resultText top-buffer">'+ entrySnippet +'</p></div> </div> </div> </div></a>'
+        $(divAdd).appendTo(".intro-body");
+      }
+    },
+    error: function(errorMessage){}
+  })
+})
